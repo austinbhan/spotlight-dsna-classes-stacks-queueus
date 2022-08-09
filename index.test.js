2bdc9f16c1399465188a09c5df19f55e7887970f
austinbhan <austinbhan@outlook.com>
2022-08-09 11:00:49 -0700

    Queue code written

--- index.test.js
+++ index.test.js
@@ -7,3 +7,12 @@ describe("Stack Class", () => {
   });
   // add more tests here...
 });
+
+const { Queue } = require("./index");
+describe("Queue Class", ()=> {
+  it("#push new item to queue", () => {
+    const queue = Queue();
+    queue.push(1);
+    expect(queue.peek())
+  });
+})
